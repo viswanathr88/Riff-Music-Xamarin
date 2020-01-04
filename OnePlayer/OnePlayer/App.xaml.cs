1433000d0fef34658c6a6df9905b9e529d237289
Viswanath Ramanan <viswanath@outlook.com>
2020-01-04 12:21:45 -0800

    Add project files.

---
+++ OnePlayer/OnePlayer/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using OnePlayer.Services;
+using OnePlayer.Views;
+
+namespace OnePlayer
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
