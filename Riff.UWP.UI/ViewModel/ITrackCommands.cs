6fe5afb58c5e72848f6b4d007a409c948877fcc6
Viswanath Ramanan <viswanath@outlook.com>
2020-07-26 13:42:20 -0700

    Fix context menu for tracks (#96)
    * Fix context menu for tracks
    
    * Add support for adding to playlist

---
+++ Riff.UWP.UI/ViewModel/ITrackCommands.cs
@@ -0,0 +1,13 @@
+﻿using Mirage.ViewModel.Commands;
+using Riff.Data;
+using System.Collections.Generic;
+
+namespace Riff.UWP.ViewModel
+{
+    public interface ITrackCommands
+    {
+        IAsyncCommand<IList<DriveItem>> Play { get; }
+
+        IAsyncCommand<DriveItem> PlayNext { get; }
+    }
+}
